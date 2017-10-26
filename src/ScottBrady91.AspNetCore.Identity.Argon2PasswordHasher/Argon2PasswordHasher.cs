3d325317fd6ef7d368d0f64f539e5f18438a352e
Scott Brady <scottbrady91@outlook.com>
2017-10-26 12:45:21 +0100

    Initial working version using Sodium.Core. Options TODO

---
+++ src/ScottBrady91.AspNetCore.Identity.Argon2PasswordHasher/Argon2PasswordHasher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Sodium;
+
+namespace ScottBrady91.AspNetCore.Identity
+{
+    public class Argon2PasswordHasher<TUser> : IPasswordHasher<TUser> where TUser : class
+    {
+        public string HashPassword(TUser user, string password)
+        {
+            if (password == null) throw new ArgumentNullException(nameof(password));
+
+            return PasswordHash.ArgonHashString(password);
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
+        {
+            if (hashedPassword == null) throw new ArgumentNullException(nameof(hashedPassword));
+            if (providedPassword == null) throw new ArgumentNullException(nameof(providedPassword));
+
+            var isValid = PasswordHash.ArgonHashStringVerify(hashedPassword, providedPassword);
+
+            return isValid ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed;
+        }
+    }
+}
---
+++ test/ScottBrady91.AspNetCore.Identity.Argon2PasswordHasher.Tests/Argon2PasswordHasherTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Sodium;
+using Xunit;
+
+namespace ScottBrady91.AspNetCore.Identity.BCryptPasswordHasher.Tests
+{
+    public class BCryptPasswordHasherTests
+    {
+        [Fact]
+        public void HashPassword_WithDefaultSettings_ExpectVerifiableHash()
+        {
+            var password = Guid.NewGuid().ToString();
+
+            var hasher = new Argon2PasswordHasher<string>();
+            var hashedPassword = hasher.HashPassword("", password);
+
+            PasswordHash.ArgonHashStringVerify(hashedPassword, password).Should().BeTrue();
+        }
+        
+        [Fact]
+        public void VerifyHashedPassword_WithDefaultSettings_ExpectSuccess()
+        {
+            var password = Guid.NewGuid().ToString();
+            var hashedPassword = PasswordHash.ArgonHashString(password);
+
+            var hasher = new Argon2PasswordHasher<string>();
+
+            hasher.VerifyHashedPassword("", hashedPassword, password).Should().Be(PasswordVerificationResult.Success);
+        }
+        
+        [Fact]
+        public void VerifyHashedPassword_WhenSuppliedPasswordDoesNotMatch_ExpectFailure()
+        {
+            var password = Guid.NewGuid().ToString();
+            var hashedPassword = PasswordHash.ArgonHashString(Guid.NewGuid().ToString());
+
+            var hasher = new Argon2PasswordHasher<string>();
+
+            hasher.VerifyHashedPassword("", hashedPassword, password).Should().Be(PasswordVerificationResult.Failed);
+        }
+    }
+}
